eb16f076072d9078b4da5dd2d01af41b21b4b3db
abdurrakib321 <abdur@zendot.in>
2025-01-06 17:41:37 +0530

    logic changes

---
+++ src/main/java/com/zendot/auth/jwt/ZenAuthenticationToken.kt
@@ -0,0 +1,4 @@
+package com.zendot.auth.jwt
+
+class ZenAuthenticationToken {
+}
--- src/main/java/com/zendot/auth/service/TokenProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.zendot.auth.service
-
-import com.zendot.auth.model.ZenUser
-
-interface TokenProvider {
-    fun generateToken(user: ZenUser): String
-}
